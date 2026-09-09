a4abec031777104ec616b6dca0fee761c1fff296
陈 啥 <wutongszmc@163.com>
2026-09-09 15:26:44 +0800

    Fix IF_ID timing issues

--- src/core/pipe_if_id.v
+++ src/core/pipe_if_id.v
@@ -6,20 +6,30 @@ module pipe_if_id(
 
     input wire [31:0] pc,
     input wire [31:0] inst,
-    output wire [31:0] if_id_inst,
+    output reg [31:0] if_id_inst,
     output reg [31:0] if_id_pc,
     output reg if_id_valid
 );
-
+    reg [31:0] pc1;
+    reg valid;
     always @(posedge clk or negedge rst_n)begin
         if(!rst_n)begin
-            if_id_valid <= 1'b0;
+            pc1<= 32'b0;
+            if_id_pc <= 32'b0;
+            if_id_valid <= 0;
+            if_id_inst <= 32'b0;
+            valid <= 1'b0;
         end else if (if_id_flush)begin
-            if_id_valid <= 1'b0;
+            valid <= 1'b0;
+            if_id_valid <= 0;
         end else if(!stall)begin
-            if_id_pc <= pc;
-            if_id_valid <= 1'b1;
+            pc1 <= pc;
+            if_id_pc <= pc1;
+            valid <= 1'b1;
+            if_id_valid <= valid;
+
+            if_id_inst <= inst;
         end
     end
-    assign if_id_inst = inst;
+
 endmodule
